0f8d4ae3f163383a4f635494d2ad23cba4f39037
Rafael Lima <rafaellima@Laptop-de-Rafael.local>
2024-11-18 19:31:32 -0300

    Organizando uma palavra errada

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>Sophia cobb é um bebezinho e Batatinha.</h1>
-    
+    return <h1>Sophia cobb é um bebezinho, uma batatinha e uma fofuxa do papai</h1>
 }
 
 export default Home;
